334e3535cd22414474a92a0298ebaaaf31a09aa5
marshall <nayyaramarshall@gmail.com>
2025-09-04 17:35:18 +0700

    update landing page & create login page for admin

---
+++ app/admin/login/page.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { ArrowLeft } from 'lucide-react';
+
+export default function AdminLoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+
+  const handleAdminLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (username === 'admin' && password === '1234') {
+      router.push('/admin/dashboard');
+    } else {
+      alert('Username atau password salah!');
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+      <div className="bg-white p-8 rounded-2xl shadow-lg w-full max-w-md">
+        {/* Logo */}
+        <img src="/logopu.png" alt="Logo Perusahaan" width="50" className="mx-auto mb-4" />
+
+        {/* Header */}
+         <h1 className="text-[20px] font-bold text-center mb-6 text-gray-800">Login Admin<br />
+        <span className="text-gray-500 text-base font-normal">BMN Monitoring Dashboard</span>
+        </h1>
+
+        {/* Form */}
+        <form onSubmit={handleAdminLogin}>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="w-full border border-gray-300 rounded-lg p-2 mb-3 focus:outline-none focus:ring-2 focus:ring-blue-400"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full border border-gray-300 rounded-lg p-2 mb-4 focus:outline-none focus:ring-2 focus:ring-blue-400"
+          />
+          <button
+            type="submit"
+            className="w-full font-semibold bg-green-500 text-white py-2 rounded-lg hover:bg-green-600 cursor-pointer">Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,39 @@
-// app/page.tsx
 'use client';
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 export default function LoginPage() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
   const router = useRouter();
 
-  const handleAdminLogin = (e: React.FormEvent) => {
-    e.preventDefault();
-    // nanti tambahkan API auth admin
-    if (username === 'admin' && password === '1234') {
-      router.push('/admin/dashboard');
-    } else {
-      alert('Username atau password salah!');
-    }
-  };
-
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="min-h-screen flex items-center justify-center bg-blue-100">
       <div className="bg-white p-8 rounded-2xl shadow-lg w-full max-w-md">
-        <h1 className="text-2xl font-bold text-center mb-6 text-gray-800">BMN Monitoring Dashboard</h1>
+        {/* Logo */}
+        <img src="/logopu.png" alt="Logo Perusahaan" width="50" className="mx-auto mb-4" />
 
-        {/* Pilihan User */}
+        {/* Header  */}
+        <h1 className="text-[20px] font-bold text-center mb-6 text-gray-800">BMN Monitoring Dashboard <br />
+        <span className="text-gray-500 text-base font-normal">Pusat Data dan Teknologi Informasi</span>
+        </h1>
+
+        {/* User */}
         <button
           onClick={() => router.push('/user/dashboard')}
-          className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 mb-4">Continue as User
+          className="w-full font-semibold bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 mb-4 cursor-pointer">Lanjut sebagai Pengguna
         </button>
 
-        <div className="flex items-center my-4">
+        <div className="flex items-center">
         <div className="flex-grow border-t border-gray-300"></div>
-        <span className="mx-4 text-gray-600">or login as Admin</span>
+        <span className="mx-4 text-gray-600">atau</span>
         <div className="flex-grow border-t border-gray-300"></div>
         </div>
         
-        {/* Form Admin */}
-        <form onSubmit={handleAdminLogin}>
-          <input
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className="w-full border border-gray-300 rounded-lg p-2 mb-3 focus:outline-none focus:ring-2 focus:ring-blue-400"
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full border border-gray-300 rounded-lg p-2 mb-4 focus:outline-none focus:ring-2 focus:ring-blue-400"
-          />
+        {/* Admin */}
           <button
-            type="submit"
-            className="w-full bg-green-500 text-white py-2 rounded-lg hover:bg-green-600"
-          >
-            Login as Admin
+            onClick={() => router.push('/admin/login')}
+            className="w-full font-semibold bg-green-500 text-white py-2 rounded-lg hover:bg-green-600 cursor-pointer mt-4">Login sebagai Admin
           </button>
-        </form>
       </div>
     </div>
   );
